f5f189f9ec26b794fff09d50ccdeaa99c69a0fbe
Erwin Werkman <werkman.erwin@gmail.com>
2019-05-08 22:09:24 +0200

    Messages and transitions

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,24 @@
     <b-row>
         <b-col cols="12">
             <authenticate></authenticate>
+            <transition  name="component-fade" mode="out-in">
             <div v-if="showInitializing" class="alert alert-info" role="alert">
-                <strong>Initializing...</strong>
+                <h4 class="alert-heading">Initializing...</h4>
                 <div v-for="message in loadMessages">
                     {{ message }}
                 </div>
             </div>
+
             <div v-if="connectionError" class="alert alert-danger" role="alert">
                 <strong>Oh snap!</strong> Something went wrong. Refresh the page, maybe it will work...
             </div>
+            <IdentityServer v-if="authenticating" />
             <MissingConfig v-if="missingConfig" />
+            </transition>
             <keep-alive>
-                <router-view v-if="loggedIn && finishedLoading" />
+              <transition name="component-fade" mode="out-in">
+                <router-view v-if="loggedIn && finishedLoading && !showInitializing" />
+              </transition>
             </keep-alive>
         </b-col>
     </b-row>
@@ -30,8 +36,9 @@ import Navigation from "./components/navigation";
 
 import axios from "axios";
 
-import Authenticate from "@/components/Authenticate.vue";
+import Authenticate from "@/components/Authenticate.vue"
 import MissingConfig from "@/components/tips/MissingConfig.vue"
+import IdentityServer from '@/components/messages/IdentityServer.vue'
 
 export default {
     name: "app",
@@ -41,9 +48,12 @@ export default {
         };
     },
     computed: {
-      showInitializing: function() {
-        !this.finishedLoading && !this.connectionError && !this.missingConfig
-      },
+        authenticating: function () {
+          return this.$store.state.authenticating;
+        },
+        showInitializing: function () {
+          return !this.finishedLoading && !this.connectionError && !this.missingConfig && !this.authenticating
+        },
         startedLoading: function () {
             return this.$store.state.startedLoading;
         },
@@ -63,7 +73,8 @@ export default {
     components: {
         Navigation,
         Authenticate,
-        MissingConfig
+        MissingConfig,
+        IdentityServer
     },
     created() {
         this.$store.dispatch("initConfig");
@@ -99,15 +110,8 @@ export default {
     methods: {
         authenticate: function (config) {
             if (config.IdentityServerUri) {
-                var identityUri = `${
-          config.IdentityServerUri
-        }/connect/authorize?response_type=id_token%20token&client_id=${
-          config.ClientId
-        }&redirect_uri=${
-          config.PlumberUri
-        }/auth/callback&scope=openid%20EngineAPI&nonce=plumber-${Math.floor(
-          Date.now()
-        )}`;
+                this.$store.commit("setAuthenticating", true);
+                var identityUri = `${config.IdentityServerUri}/connect/authorize?response_type=id_token%20token&client_id=${config.ClientId}&redirect_uri=${config.PlumberUri}/auth/callback&scope=openid%20EngineAPI&nonce=plumber-${Math.floor(Date.now())}`;
                 window.location = identityUri;
             }
         }
@@ -137,5 +141,20 @@ h1 {
 
 .code {
     font-family: 'Roboto Mono', monospace;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+.component-fade-enter-active, .component-fade-leave-active {
+  transition: opacity .3s ease;
+}
+.component-fade-enter, .component-fade-leave-to
+/* .component-fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
