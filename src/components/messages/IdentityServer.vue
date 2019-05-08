b987771737757f9561cb1d52657610ca6ceb3d3e
Erwin Werkman <werkman.erwin@gmail.com>
2019-05-08 22:09:24 +0200

    Messages and transitions

---
+++ src/components/messages/IdentityServer.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="alert alert-info" role="alert">
+   <h4 class="alert-heading">Redirecting you to Sitecore Identity Server...</h4>
+   <p>...so you can log on.</p>
+   <hr>
+   <p></p>
+</div>
+</template>
