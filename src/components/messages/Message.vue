af70e05c218b78c6caabffa3905bafbf4ac2b130
Erwin Werkman <werkman.erwin@gmail.com>
2019-05-09 22:29:49 +0200

    Added error handling and messages

---
+++ src/components/messages/Message.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="alert alert-info" role="alert">
+    <h4 class="alert-heading">{{heading}}</h4>
+    <p v-html="message"></p>
+</div>
+</template>
+
+<script>
+export default {
+  props: ["heading", "message"]
+
+}
+</script>
+
+<style>
+
+</style>
